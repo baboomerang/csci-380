18117269ce82a4674ce933d8b36e0d33237deb64
baboomerang <irodrigue101@gmail.com>
2021-02-13 13:17:45 -0500

    updated start counter

--- 2-counter/app.js
+++ 2-counter/app.js
@@ -1,8 +1,12 @@
 // set inital value to zero
-let count = 0;
+let count = 2;
+
 // select value and buttons
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
+
+console.log(btns)
+
 
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
