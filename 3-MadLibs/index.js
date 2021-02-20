a674f7703f3313d0ba81aea8ff906b055334dcc1
baboomerang <irodrigue101@gmail.com>
2021-02-20 16:52:30 -0500

    removed the "remove" button for now

--- 3-MadLibs/index.js
+++ 3-MadLibs/index.js
@@ -1,14 +1,16 @@
 // Madlibs!
 
 
+//database
 const companies = ["Uber", "Lyft", "Google", "Apple", "SpaceX", "Tinder"]
 const animals = ["Aardvark", "Blue Footed Booby", "Flying Dragon", "Giraffe Weevil", 
     "Hammerhead Slug", "Komodo Dragon", "Naked Mole Rat", "Okapi", "Red Panda"]
 const vowels = ["A", "E", "I", "O", "U"]
 
 
-const visibleString = document.querySelector("#containerString");
+//query buttons
 const btns = document.querySelectorAll(".btn"); 
+
 
 function generateMadlib() {
     const companyName = companies[Math.floor(Math.random() * companies.length)]
@@ -16,25 +18,38 @@ function generateMadlib() {
     const companyString = "My company is a"
         + ((vowels.indexOf(firstLetter) > -1) ? "n " : " ")
         + companyName + " for " + animals[Math.floor(Math.random() * animals.length)]
-        + "s!\n"
+        + "s! \n"
     
     return companyString;
 }
-
-btns.forEach(function(btn) {
-    btn.addEventListener("click", function(event) {
+   
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (event) {
         const styles = event.currentTarget.classList;
-        if (styles.contains("increase")) {
-            containerString += generateMadlib();
-        } else if (styles.contains("decrease")) {
-            var draft = containerString.slice(0, -1);
-            var pos = containerString.lastIndexOf("\n");
-            if (pos) {
-                var result = draft.slice(0, pos);
-                containerString = result;
-            }
+        
+        if (styles.contains("generate")) {
+            containerString.textContent = generateMadlib();
         } else {
-            containerString = "Generate MadLib";
+            containerString.textContent = "Reset! Strings have been reset.";
+
+            setTimeout(function() {
+                containerString.textContent = "Generate new Madlib by clicking \"Generate\"";
+            }, 1000);
         }
     });
 });
+
+/* See if somehow I can append strings to the containerString with newlines
+ * then the below method could work.
+ * Untested - but implemented below incase there is a solution found in the future
+ */
+ /*if (styles.contains("decrease")) {
+      var draft = containerString.textContent.slice(0, -1);
+      var pos = draft.lastIndexOf("\n");
+      if (pos) {
+          var result = draft.slice(0, pos);
+          containerString.textContent = result;
+      }
+ }
+ */
+  
